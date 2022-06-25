6ee52c3f76864e920c5e401f1bf54273704dfdd6
nilanaga2 <nilangak107@gmail.com>
2022-06-26 02:54:52 +0530

    implementation started

---
+++ BackEnd/src/main/java/lk/ijse/WebAppInitializer.java
@@ -0,0 +1,15 @@
+package lk.ijse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Tharindu Nilanga
+ * @created 6/26/2022
+ */
+@SpringBootApplication
+public class WebAppInitializer {
+    public static void main(String[] args) {
+        SpringApplication.run(WebAppInitializer.class);
+    }
+}
---
+++ BackEnd/src/main/java/lk/ijse/config/WeAppConfig.java
@@ -0,0 +1,9 @@
+package lk.ijse.config;
+
+/**
+ * @author Tharindu Nilanga
+ * @created 6/26/2022
+ */
+public class WeAppConfig {
+}
+   /* git remote add origin https://github.com/TharinduNilanga/SPA_with_Spring_Boot.git*/
